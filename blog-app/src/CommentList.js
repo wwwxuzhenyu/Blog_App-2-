01234f9ea71367cfe05e4d7f3187547d0db4a734
Zhenyu_Xu <wwwxuzhenyu@126.com>
2019-07-28 11:18:35 +0800

    增加了未评论时显示当前没有评论的功能

--- blog-app/src/CommentList.js
+++ blog-app/src/CommentList.js
@@ -1,33 +1,37 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Comment from './Comment';
 import PropTypes from 'prop-types'
 
 
-class CommentList extends Component{
-    static propTypes ={
-        comments:PropTypes.array,
-        onDeleteComment:PropTypes.func
-    }
-    
-    static defaultProps={
-        comments:[]
+class CommentList extends Component {
+    static propTypes = {
+        comments: PropTypes.array,
+        onDeleteComment: PropTypes.func
     }
 
-    handleDeleteComment(index){
-        if(this.props.onDeleteComment){
-          console.log(index)
+    static defaultProps = {
+        comments: []
+    }
+
+    handleDeleteComment(index) {
+        if (this.props.onDeleteComment) {
+            console.log(index)
             this.props.onDeleteComment(index)
         }
-      }                                              
+    }
 
-    render(){        
-        return(
+    render() {
+        return (
             <div>
-                {this.props.comments.map((comment,i)=><Comment
-                                                       comment={comment}
-                                                       key={i}
-                                                       index={i}
-                                                       onDeleteComment={this.handleDeleteComment.bind(this)}/>)}
+                {this.props.comments.length
+                    ? this.props.comments.map((comment, i) => {
+                        return (<Comment
+                            comment={comment}
+                            key={i}
+                            index={i}
+                            onDeleteComment={this.handleDeleteComment.bind(this)} />)
+                    })
+                    : <div>目前还没有评论，快来抢沙发叭~~~~</div>}
             </div>
         )
     }
